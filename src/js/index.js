19a5a1668427e38281ace16a03c81768e75c5cd9
eduardo1804 <145880503+eduardo1804@users.noreply.github.com>
2023-10-11 20:45:22 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem")
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+
+        desselecionarbotao();
+
+        botao.classList.add("selecionado")
+
+        const personagemSelecionado = document.querySelector('.personagem.selecionado')
+        personagemSelecionado.classList.remove("selecionado")
+
+        personagens[indice].classList.add("selecionado")
+    });
+});
+function desselecionarbotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarbotao() {
+    desselecionarBotao();
+}
+
+function desselecionarBotao() {
+    desselecionarbotao();
+}
+
+function desselecionarbotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
