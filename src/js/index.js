0be68d0bedc775385d45b60b4970a00a7931185d
eduardo1804 <145880503+eduardo1804@users.noreply.github.com>
2023-10-11 21:48:20 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,34 @@
 const botoes = document.querySelectorAll(".botao");
-const personagens = document.querySelectorAll(".personagem");
+const personagens = document.querySelectorAll(".personagem")
 
 botoes.forEach((botao, indice) => {
-	botao.addEventListener("click", () => {
-		desselecionarBotao();
-		desselecionarPersonagem();
+    botao.addEventListener("click", () => {
 
-		botao.classList.add("selecionado");
-		personagens[indice].classList.add("selecionado");
-	});
+        desselecionarbotao();
+
+        botao.classList.add("selecionado")
+
+        const personagemSelecionado = document.querySelector('.personagem.selecionado')
+        personagemSelecionado.classList.remove("selecionado")
+
+        personagens[indice].classList.add("selecionado")
+    });
 });
+function desselecionarbotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
 
-function desselecionarPersonagem() {
-	const personagemSelecionado = document.querySelector(".personagem.selecionado");
-	personagemSelecionado.classList.remove("selecionado");
+function desselecionarbotao() {
+    desselecionarBotao();
 }
 
 function desselecionarBotao() {
-	const botaoSelecionado = document.querySelector(".botao.selecionado");
-	botaoSelecionado.classList.remove("selecionado");
+    desselecionarbotao();
 }
+
+function desselecionarbotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
